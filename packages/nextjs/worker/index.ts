ff992ffd927ce7bbfaf42ed32f773531fda7212c
Shiv Bhonde <shivbhonde04@gmail.com>
2023-08-26 21:13:59 +0530

    setup basic custom webworker

---
+++ packages/nextjs/worker/index.ts
@@ -0,0 +1,41 @@
+declare let self: ServiceWorkerGlobalScope;
+
+// To disable all workbox logging during development, you can set self.__WB_DISABLE_DEV_LOGS to true
+// https://developers.google.com/web/tools/workbox/guides/configure-workbox#disable_logging
+//
+// self.__WB_DISABLE_DEV_LOGS = true
+
+// listen to message event from window
+self.addEventListener("message", event => {
+  console.log(event?.data);
+});
+
+self.addEventListener("push", event => {
+  // const data = JSON.parse(event?.data?.text() || "{}");
+  event?.waitUntil(
+    self.registration.showNotification("Test Notification", {
+      body: event?.data?.text(),
+      icon: "/logo.svg",
+    }),
+  );
+});
+
+self.addEventListener("notificationclick", event => {
+  event?.notification.close();
+  event?.waitUntil(
+    self.clients.matchAll({ type: "window", includeUncontrolled: true }).then(function (clientList) {
+      if (clientList.length > 0) {
+        let client = clientList[0];
+        for (let i = 0; i < clientList.length; i++) {
+          if (clientList[i].focused) {
+            client = clientList[i];
+          }
+        }
+        return client.focus();
+      }
+      return self.clients.openWindow("/");
+    }),
+  );
+});
+
+export {};
